27b5fbbf6899e34bee6eca3df111381c7afe5a13
jeremyqzt <jq357@hotmail.com>
2020-11-29 18:17:37 -0500

    word 2 vec using gensim

--- ideas/jsonHelpers.py
+++ ideas/jsonHelpers.py
@@ -1,20 +1,26 @@
 import json
 from os import path
+import math
 
 def writeJson(fName, data):
     with open(fName, 'w') as json_file:
         json.dump(data, json_file, indent=4)
 
-def cleanUpDataset(fDir, fName):
+def cleanUpDataset(fDir, fName, limit=math.inf):
     reviews = []
     cleanName = "cleanData"
     if path.exists("./%s/%s" % (cleanName, fName)):
         return
+    num = 0
 
     with open("./%s/%s" % (fDir, fName)) as f:
         for line in f:
             temp = json.loads(line)
             temp['reviewText'] = temp['reviewText'].lower()
             reviews.append(json.loads(line))
+            num = num + 1
+            if num > limit:
+                break
+
         jsonReviews = { "reviews": reviews }
         writeJson("./%s/%s" % (cleanName, fName), jsonReviews)
--- ideas/main.py
+++ ideas/main.py
@@ -3,7 +3,32 @@ import numpy as np
 import tensorflow as tf
 import time
 from jsonHelpers import cleanUpDataset
+from nltk.tokenize import sent_tokenize, word_tokenize 
+import warnings 
+import nltk
+warnings.filterwarnings(action = 'ignore') 
+  
+import gensim 
+from gensim.models import Word2Vec
+from nltk.tokenize import sent_tokenize, word_tokenize 
+nltk.download('punkt')
 
 start_time = time.time()
 cleanUpDataset("data", "Electronics_5.json")
 print("%s data read time seconds" % (time.time() - start_time))
+
+reviewDict = {}
+
+with open("./cleanData/Electronics_5.json") as jsonfile:
+    reviewDict = json.load(jsonfile)
+
+data = []
+
+for review in reviewDict['reviews']:
+    reviewItem = sent_tokenize(review['reviewText'])
+    for word in reviewItem:
+        data.append(word_tokenize(word))
+
+model = gensim.models.Word2Vec(data, min_count = 1, size = 100, window = 5, sg = 1) 
+
+print("Similarity", model.similarity('gps', 'bedframe')) 
